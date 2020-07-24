dc3ccebc9072158fe68f7515ce9da238b71a77d2
Sukeri Sukianto <sukeri.sukianto@arcor.de>
2020-07-24 11:00:28 +0200

    Sukianto Commit 1

--- TestGradle/src/main/java/sukeri/test/data/CustomerDTO.java
+++ TestGradle/src/main/java/sukeri/test/data/CustomerDTO.java
@@ -3,11 +3,18 @@ package sukeri.test.data;
 public class CustomerDTO {
 	private String name;
 	private String firstName;
+	
 	public String getFirstName() {
 		return firstName;
 	}
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
 	}
 	
 	
